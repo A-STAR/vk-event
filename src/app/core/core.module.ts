be2469afcd744dbc84105c9eceaa50311b481658
A-STAR <andreysuperstar@gmail.com>
2017-08-29 23:36:54 +0300

    Add page-not-found.component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [],
+  declarations: [PageNotFoundComponent],
   providers: [],
-  exports: []
+  exports: [PageNotFoundComponent]
 })
 export class CoreModule { }
