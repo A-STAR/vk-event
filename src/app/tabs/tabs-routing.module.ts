0e924a41a7f4a6f74e53a0196075e2d386fe2ee8
A-STAR <andreysuperstar@gmail.com>
2017-08-30 21:40:43 +0300

    add info.component to tabs-routing.module

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { TabsComponent } from './tabs.component';
+import { InfoComponent } from './info/info.component';
 
 const routes: Routes = [
   {
     path: '',
     component: TabsComponent,
-    children: []
+    children: [
+      {
+        path: '',
+        component: InfoComponent
+      }
+    ]
   }
 ];
 
