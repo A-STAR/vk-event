2de4884506d66182c0bdbba7c06412da187233ce
A-STAR <andreysuperstar@gmail.com>
2017-09-05 21:58:14 +0300

    Set route on intial load for Info tab active state

--- src/app/tabs/tabs.component.ts
+++ src/app/tabs/tabs.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Location } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'tabs',
@@ -8,11 +8,11 @@ import { Location } from '@angular/common';
 })
 export class TabsComponent implements OnInit {
 
-  constructor(private location: Location) { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
-    this.location
-      .go('/');
+    this.router
+      .navigate(['/']);
   }
 
 }
