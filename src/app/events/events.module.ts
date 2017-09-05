e9d12ad2a828a56c585e8018b4ece32ce2fee325
A-STAR <andreysuperstar@gmail.com>
2017-09-05 20:09:25 +0300

    Add Incomint & Outgoing page to Events page

--- src/app/events/events.module.ts
+++ src/app/events/events.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { EventsRoutingModule } from './events-routing.module';
 import { EventsComponent } from './events.component';
+import { IncomingComponent } from './incoming/incoming.component';
+import { OutgoingComponent } from './outgoing/outgoing.component';
 
 @NgModule({
   imports: [
     CommonModule,
     EventsRoutingModule
   ],
-  declarations: [EventsComponent]
+  declarations: [EventsComponent, IncomingComponent, OutgoingComponent]
 })
 export class EventsModule { }
