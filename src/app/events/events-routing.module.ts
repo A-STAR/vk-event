468a6c1256894a4d46426e547471d4536b8accce
A-STAR <andreysuperstar@gmail.com>
2017-09-05 20:20:36 +0300

    Add Accepted, Incoming, Outgoing pages to Events routing

--- src/app/events/events-routing.module.ts
+++ src/app/events/events-routing.module.ts
@@ -1,11 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { EventsComponent } from './events.component';
+import { AcceptedComponent } from './accepted/accepted.component';
+import { IncomingComponent } from './incoming/incoming.component';
+import { OutgoingComponent } from './outgoing/outgoing.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: EventsComponent
+    component: EventsComponent,
+    children: [
+      {
+        path: '',
+        component: AcceptedComponent
+      },
+      {
+        path: 'incoming',
+        component: IncomingComponent
+      },
+      {
+        path: 'outgoing',
+        component: OutgoingComponent
+      }
+    ]
   }
 ];
 
