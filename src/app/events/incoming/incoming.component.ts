adf6f247f951beefdd3e91dee8e3000d72962fc7
A-STAR <andreysuperstar@gmail.com>
2017-09-08 02:56:59 +0300

    Copy layout to Incoming page

--- src/app/events/incoming/incoming.component.ts
+++ src/app/events/incoming/incoming.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'incoming',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IncomingComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  navigate(event) {
+    this.router
+      .navigate(['/events']);
+    console.log('navigate');
   }
 
 }
