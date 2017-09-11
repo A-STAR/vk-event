83b61752956cef90210ad099e4eec61a6b47064f
A-STAR <andreysuperstar@gmail.com>
2017-09-11 04:57:48 +0300

    Add layout to people.component

--- src/app/people/people.component.ts
+++ src/app/people/people.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'people',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PeopleComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  navigate(event) {
+    console.log('navigate');
+    this.router
+      .navigate(['people']);
   }
 
 }
