76570aaef885bef7b67cc0e6bde240357ee68b6f
A-STAR <andreysuperstar@gmail.com>
2017-09-13 11:12:55 +0300

    Fix float button

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -8,24 +8,28 @@ import { Component, OnInit } from '@angular/core';
 export class AboutComponent implements OnInit {
 
   go: boolean;
-  showButton: boolean;
   buttonText: string;
   buttonClass: string;
+  showButton: boolean;
 
   constructor() {}
 
   ngOnInit() {
+    this.show();
+  }
+
+  show() {
     this.go = false;
-    this.showButton = !this.go;
     this.buttonText = 'Пойти';
     this.buttonClass = 'btn-primary';
+    this.showButton = !this.go;
   }
 
   goTo(event) {
     this.go = true;
-    setTimeout(() => this.showButton = false, 3000);
     this.buttonText = 'Вы идёте';
     this.buttonClass = 'btn-success';
+    setTimeout(() => this.showButton = false, 3000);
   }
 
 }
--- src/app/profile/search/search.component.ts
+++ src/app/profile/search/search.component.ts
@@ -7,13 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  buttonText: string;
+  buttonClass: string;
+  showButton: boolean;
+  buttonTimeout: any;
+
+  constructor() {}
 
   ngOnInit() {
+    this.show();
+  }
+
+  show() {
+    this.buttonText = 'Сохранить';
+    this.buttonClass = 'btn-primary';
+    this.showButton = true;
+  }
+
+  input(event) {
+    if (this.buttonClass === 'btn-primary') {
+      return;
+    }
+    if (this.showButton) {
+      clearTimeout(this.buttonTimeout);
+    }
+    this.show();
   }
 
   submit(event) {
     event.preventDefault();
+    this.save();
+  }
+
+  save() {
+    this.buttonText = 'Сохранено';
+    this.buttonClass = 'btn-success';
+    this.buttonTimeout = setTimeout(() => this.showButton = false, 3000);
   }
 
 }
