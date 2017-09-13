dc60fc7d45c3205d2ac6a54825caa95c0b32bc3e
A-STAR <andreysuperstar@gmail.com>
2017-09-13 04:59:23 +0300

    Refactor tabs, & a\dd Info, Search, Time components

--- src/app/profile/profile-routing.module.ts
+++ src/app/profile/profile-routing.module.ts
@@ -1,12 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProfileComponent } from './profile.component';
+import { InfoComponent } from './info/info.component';
+import { SearchComponent } from './search/search.component';
+import { TimeComponent } from './time/time.component';
 
 const routes: Routes = [
   {
     path: '',
     component: ProfileComponent,
-    children: []
+    children: [
+      {
+        path: '',
+        component: InfoComponent
+      },
+      {
+        path: 'search',
+        component: SearchComponent
+      },
+      {
+        path: 'time',
+        component: TimeComponent
+      }
+    ]
   }
 ];
 
--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -3,12 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { ProfileRoutingModule } from './profile-routing.module';
 import { ProfileComponent } from './profile.component';
+import { InfoComponent } from './info/info.component';
+import { SearchComponent } from './search/search.component';
+import { TimeComponent } from './time/time.component';
 
 @NgModule({
   imports: [
     CommonModule,
     ProfileRoutingModule
   ],
-  declarations: [ProfileComponent]
+  declarations: [ProfileComponent, InfoComponent, SearchComponent, TimeComponent]
 })
 export class ProfileModule { }
