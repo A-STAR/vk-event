87fd72ffb3a371749b661390dca42a3b77e1c594
A-STAR <andreysuperstar@gmail.com>
2017-09-20 06:14:47 +0300

    Complete Registration Step 1

--- src/app/registration/step-1/step-1.component.ts
+++ src/app/registration/step-1/step-1.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'step-1',
+  selector: 'step1',
   templateUrl: './step-1.component.html',
   styleUrls: ['./step-1.component.scss']
 })
 export class Step1Component implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  submit(event) {
+    event.preventDefault();
+    this.next();
+  }
+
+  next() {
+    this.router
+      .navigate(['registration/step-2']);
   }
 
 }
