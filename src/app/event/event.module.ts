272fc1c80b9f63dde7f17ba52b98f77e47a50c6d
A-STAR <andreysuperstar@gmail.com>
2017-09-24 16:07:55 +0300

    Add Event Modal

--- src/app/event/event.module.ts
+++ src/app/event/event.module.ts
@@ -4,12 +4,13 @@ import { CommonModule } from '@angular/common';
 import { EventRoutingModule } from './event-routing.module';
 import { EventComponent } from './event.component';
 import { EventEditComponent } from './event-edit/event-edit.component';
+import { EventModalComponent } from './shared/event-modal.component';
 
 @NgModule({
   imports: [
     CommonModule,
     EventRoutingModule
   ],
-  declarations: [EventComponent, EventEditComponent]
+  declarations: [EventComponent, EventEditComponent, EventModalComponent]
 })
 export class EventModule { }
---
+++ src/app/event/shared/event-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'event-modal',
+  templateUrl: './event-modal.component.html',
+  styleUrls: ['./event-modal.component.scss']
+})
+export class EventModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
