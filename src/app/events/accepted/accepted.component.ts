5e71b4339766d84f365842b52018ae1c01407061
A-STAR <andreysuperstar@gmail.com>
2017-09-24 12:46:05 +0300

    Fix code style

--- src/app/events/accepted/accepted.component.ts
+++ src/app/events/accepted/accepted.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'accepted',
@@ -8,7 +8,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class AcceptedComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute, private router: Router) { }
+  constructor(private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
   }
